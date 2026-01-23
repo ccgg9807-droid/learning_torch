b8cedfb620ee0197bf26847cc82653a82b4614d1
ccgg9807-droid <ccgg9807@gmail.com>
2026-01-24 06:17:39 +0800

    周五的内容

---
+++ 神经网络/Sequential and Module.py
@@ -0,0 +1,171 @@
+import torch
+import numpy as np
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torch import nn
+
+#绘制决策边界函数 model是模型 x是数据坐标点（x,y） y是对应数据的标签label 正负1
+def plot_decision_boudnary(model,x,y):
+    #绘制数据边界 padding=1 防止数据出现在边界 要注意的是这里的y_min和y_max 都是参数x里的数据
+    x_min,x_max = x[:,0].min()- 1, x[:,0].max() + 1
+    y_min,y_max = x[:,1].min()- 1, x[:,1].max() + 1
+    h = 0.01
+
+    xx , yy = np.meshgrid(np.arange(x_min,x_max,h),np.arange(y_min,y_max,h))
+    #np.c_[xx.ravel(),yy.ravel()] 就把数据变成了坐标一一对应的形式
+    #[[x0,y0]
+    #[x1,y1]
+    #[x2,y2]]
+    #z = 模型后对数据点分分类情况
+    z = model(np.c_[xx.ravel(),yy.ravel()])
+    z = z.reshape(xx.shape)
+
+    plt.contourf(xx,yy,z,cmap = plt.cm.Spectral)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.scatter(x[:,0],x[:,1],c=y.reshape(-1),cmap = plt.cm.Spectral)
+    plt.show()
+
+#数据
+np.random.seed(1)
+m = 400 
+N = int(m/2)
+D = 2
+x = np.zeros((m,D))
+y = np.zeros((m,1),dtype= 'uint8')
+a = 4
+
+for j in range(2):
+    ix = range(N * j, N * (j+1))
+    t = np.linspace(j * 3.12,(j+1) * 3.12,N ) + np.random.randn(N) * 0.2
+    r = a * np.sin(4 * t) +np.random.randn(N) * 0.2
+    x[ix] = np.c_[r * np.sin(t),r * np.cos(t)]
+    y[ix] = j
+
+plt.scatter(x[:, 0], x[:, 1], c=y.reshape(-1), s=40, cmap=plt.cm.Spectral)
+plt.show()
+#转化成张量
+x = torch.from_numpy(x).float()
+y = torch.from_numpy(y).float()
+
+#sequential 直接代替w1 b1 w2 b2
+seq_set  = nn.Sequential(
+    nn.Linear(2,4),
+    nn.Tanh(),
+    nn.Linear(4,1)
+)
+#w = nn.Parameter(torch.randn(n,m))  让optim记住参数
+param = seq_set.parameters()
+
+optimizer = torch.optim.SGD(param,lr=1.)
+criterion = nn.BCEWithLogitsLoss()
+for e in range(10000):
+    y_ = seq_set(Variable(x))
+    loss = criterion(y_,Variable(y))
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if((e+1)%1000 ==0):
+        print("epoch : {} loss : {}".format(e+1,loss.data))
+
+
+def plot_sequential(x):
+    out = F.sigmoid(seq_set(Variable(torch.from_numpy(x).float()))).data.numpy()
+    out = (out > 0.5)*1
+    return out
+
+plot_decision_boudnary(lambda x:plot_sequential(x),x.numpy(),y.numpy())
+plt.show()
+
+
+
+'''
+#多层神经网络
+w1 = nn.Parameter(torch.randn(2,4)*0.01)
+b1 = nn.Parameter(torch.zeros(4) )
+
+w2 = nn.Parameter(torch.randn(4,1)* 0.01)
+b2 = nn.Parameter(torch.zeros(1))
+
+def two_networks(x):
+    x1 = torch.mm(x,w1) + b1
+    y1 = F.tanh(x1)
+    x2 = torch.mm(y1,w2)+b2
+    return x2
+
+optimizer = torch.optim.SGD([w1,w2,b1,b2],lr=1.)
+
+criterion = nn.BCEWithLogitsLoss()
+
+for e in range(10000):
+    y_ = two_networks(Variable(x))
+    loss = criterion(y_,Variable(y))
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if((e+1)%1000 ==0):
+        print("epoch : {} loss : {}".format(e+1,loss.data))
+
+def plot_twonetwork(x):
+    x = torch.from_numpy(x).float()
+    x = Variable(x)
+    y_ = two_networks(x)
+    out = F.sigmoid(y_)
+    out =(out>0.5)*1
+    return out
+
+plot_decision_boudnary(lambda x:plot_twonetwork(x),x.numpy(),y.numpy())
+plt.show()
+
+'''
+'''
+#logistic 方法 
+np.random.seed(1)
+m = 400 
+N = int(m/2)
+D = 2
+x = np.zeros((m,D))
+y = np.zeros((m,1),dtype= 'uint8')
+a = 4
+
+for j in range(2):
+    ix = range(N * j, N * (j+1))
+    t = np.linspace(j * 3.12,(j+1) * 3.12,N ) + np.random.randn(N) * 0.2
+    r = a * np.sin(4 * t) +np.random.randn(N) * 0.2
+    x[ix] = np.c_[r * np.sin(t),r * np.cos(t)]
+    y[ix] = j
+
+plt.scatter(x[:, 0], x[:, 1], c=y.reshape(-1), s=40, cmap=plt.cm.Spectral)
+plt.show()
+
+
+
+w = nn.Parameter(torch.randn(2,1))
+b = nn.Parameter(torch.zeros(1))
+
+optimizer = torch.optim.SGD([w,b],lr=1e-1)
+
+def logistic_regression(x):
+    return torch.mm(x,w)+b
+
+criterion = nn.BCEWithLogitsLoss()
+
+for e in range(100):
+    y_ = logistic_regression(Variable(x))
+    loss = criterion(y_,Variable(y))
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if((e+1)%20 ==0):
+        print("epoch : {} loss : {}".format(e+1,loss.data))
+
+def plot_logistic(x):
+    x = torch.from_numpy(x).float()
+    y_ = logistic_regression(Variable(x))
+    y_ = F.sigmoid(y_)
+    out = (y_ > 0.5) * 1
+    return  out.data.numpy()
+
+plot_decision_boudnary(lambda x:plot_logistic(x),x.numpy(),y.numpy())
+'''
---
+++ 神经网络/logstic.py
@@ -0,0 +1,135 @@
+import torch
+import numpy as np
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torch import nn
+#数据路径输入
+with open('/home/cg/深度学习/神经网络/data.txt','r') as f:
+    datalist = [ i.split('\n')[0].split(',') for i in f.readlines()]
+    data = [(float(i[0]),float(i[1]),float(i[2])) for i in datalist]
+
+#标准化 每个值不大于1
+x0_max = max(i[0] for i in data)
+x1_max = max(i[1] for i in data)
+data = [( i[0]/x0_max , i[1]/x1_max , i[2]) for i in data]
+
+#不同的点分类
+x0 = list( filter ( lambda x : x[-1] == 0.0, data))
+x1 = list( filter ( lambda x : x[-1] == 1.0, data))
+plot_x0 = list( i[0] for i in x0 )
+plot_y0 = list( i[1] for i in x0)
+plot_x1 = list (i[0] for i in x1)
+plot_y1 = list (i[1] for i in x1)
+
+#data转化成numpy的形式
+np.data = np.array(data,dtype= 'float32')
+#再转化成torch
+#数据x=（w1,w2）
+x_data = torch.from_numpy(np.data[:,0:2])
+#标签|y|=1
+y_data = torch.from_numpy(np.data[:,-1]).unsqueeze(1)
+
+#转化成varible
+x_data = Variable(x_data)
+y_data = Variable(y_data)
+
+
+w = nn.Parameter(torch.randn(2,1))
+b = nn.Parameter(torch.zeros(1))
+
+def logstic_regression(x):
+    return F.sigmoid(torch.mm(x,w)+b)
+
+def binary_loss(y_,y_data):
+    logits = torch.mean((y_data * y_.clamp(1e-12).log() + (1 - y_data) * (1 - y_).clamp(1e-12).log() )) #clamp函数为了保证损失值始终有一个保底的正数下限不至于出现梯度消失 的现象
+    return -logits
+
+optimizer = torch.optim.SGD([w,b],lr= 1.)
+
+import time
+start = time.time()
+for e in range(1000):
+    y_ = logstic_regression(x_data)
+    loss = binary_loss(y_,y_data)
+
+    optimizer.zero_grad()
+    loss.backward()
+
+    optimizer.step()
+
+    mask = y_.ge(0.5).float()
+    acc =(mask == y_data).sum().data/y_data.shape[0]
+    if((e+1)%20 ==0):
+        print("epoch : {} loss : {}".format(e+1,loss.data,acc))
+during = time.time()-start
+
+
+
+'''
+#权重w和b
+w = Variable(torch.randn(2,1),requires_grad= True)
+b = Variable(torch.zeros(1),requires_grad= True)
+
+#sigmoid 函数
+def logstic_regression(x):
+    return F.sigmoid(torch.mm(x,w)+b)
+
+#显示初始分类面
+w0 = w[0].data
+w1 = w[1].data
+b0 = b.data
+plot_x = np.arange(0.2,1,0.01)
+plot_y = (- w0 * plot_x - b0) / w1
+plt.figure(1)
+plt.plot(plot_x,plot_y,'g',label = 'cutting line')
+plt.plot(plot_x0,plot_y0,'bo',label = 'x0')
+plt.plot(plot_x1,plot_y1,'ro',label = 'x1')
+plt.legend()
+plt.show()
+
+# loss = -(y*lny_ + （1-y）* ln(1-y_))  y是数据经过sigmoid函数处理过后的
+def binary_loss(y_,y_data):
+    logits = torch.mean((y_data * y_.clamp(1e-12).log() + (1 - y_data) * (1 - y_).clamp(1e-12).log() )) #clamp函数为了保证损失值始终有一个保底的正数下限不至于出现梯度消失 的现象
+    return -logits
+
+y_ = logstic_regression(x_data)
+loss = binary_loss(y_,y_data)
+print(loss)
+
+loss.backward()
+w.data = w.data - 0.1 * w.grad.data
+b.data = b.data - 0.1 * b.grad.data
+
+y_ = logstic_regression(x_data)
+loss = binary_loss(y_,y_data)
+print(loss)
+
+
+for i in range(1,1001):
+    y_ = logstic_regression(x_data)
+    w.grad.zero_()
+    b.grad.zero_()
+    loss = binary_loss(y_,y_data)
+    
+    loss.backward()
+    w.data = w.data - 1 * w.grad.data
+    b.data = b.data - 1 * b.grad.data
+
+    if((i+1)%20 ==0):
+        print("epoch : {} loss : {}".format(i+1,loss.data))
+
+
+w0 = w[0].data
+w1 = w[1].data
+b0 = b.data
+plot_x = np.arange(0.2,1,0.01)
+plot_y = (- w0 * plot_x - b0) / w1
+plt.figure()
+plt.plot(plot_x,plot_y,'g',label = 'cutting line')
+plt.plot(plot_x0,plot_y0,'bo',label = 'x0')
+plt.plot(plot_x1,plot_y1,'ro',label = 'x1')
+plt.legend()
+plt.show()
+'''
+
